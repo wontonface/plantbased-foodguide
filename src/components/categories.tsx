d72efae610dbd07ce9ef5bd4fa281af62299b921
Nicole Garcia <nicole@nicoleiliana.com>
2025-07-25 14:33:00 -0700

    imported radix-ui, started category filters

---
+++ src/components/categories.tsx
@@ -0,0 +1,11 @@
+import { Box, CheckboxCards, Text } from "@radix-ui/themes"
+
+function CategoryFilters() {
+    return (
+        <Box maxWidth="150px">
+            <Text>Category filters</Text>
+        </Box>
+    )
+}
+
+export default CategoryFilters
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Theme accentColor="lime" panelBackground="solid" radius="large" scaling="110%">
     <App />
+    </Theme>
   </StrictMode>,
 )
+
