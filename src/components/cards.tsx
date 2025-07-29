84e64c372aaf971a3a80f8847a7f458ff7c9186d
Nicole Garcia <nicole@nicoleiliana.com>
2025-07-29 14:27:50 -0700

    started veggie cards in cards.tsx

---
+++ src/components/cards.tsx
@@ -0,0 +1,12 @@
+import { Box, Flex, Text } from "@radix-ui/themes"
+
+function VeggieCards() {
+
+    return (
+        <Box maxWidth="900px">
+            <Text>Category filters</Text> 
+        </Box>
+    )
+}
+
+export default VeggieCards
