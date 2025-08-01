21f17b9a6182b9f12531b1a8eb5a3b6df0a27ea0
Nicole Garcia <nicole@nicoleiliana.com>
2025-07-31 17:25:48 -0700

    Update cards.tsx with card placeholder

--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Text } from "@radix-ui/themes"
+import { Avatar, Box, Card, Flex, Text } from "@radix-ui/themes"
 import { VeggieCategory, VeggieCategoryName, Frequency } from "./categories";
 
 enum VeggieName {
@@ -134,6 +134,24 @@ function VeggieCards() {
 
     return (
         <Box maxWidth="900px">
+            <Card>
+                <Flex gap="3" align="center">
+                    <Avatar
+                        size="3"
+                        src="https://unsplash.com/photos/green-broccoli-on-white-background-q7BJL1qZ1Bw"
+                        radius="full"
+                        fallback="T"
+                    />
+                    <Box>
+                        <Text as="div" size="2" weight="bold">
+                            Kale
+                        </Text>
+                        <Text as="div" size="2" color="gray">
+                            Cruciferous
+                        </Text>
+                    </Box>
+                </Flex>
+            </Card>
             <Text>Veggie Cards</Text> 
         </Box>
     )
